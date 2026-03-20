e9185d758eb958636a494d6937f74ecfee99ca30
ManeolSF <namoel.ferreira@gmail.com>
2026-03-20 20:50:32 -0300

    aula 2 variaveis e operadores

---
+++ aula02-variaveis-operadores/ex3.py
@@ -0,0 +1,4 @@
+num1 = int(input("Digite um numero inteiro: "))
+num2 = int(input("Digite outro numero inteiro: "))
+
+print("A soma é igual à: ", num1 + num2)
---
+++ aula02-variaveis-operadores/exercicio 2.py
@@ -0,0 +1,4 @@
+dia = str(input("Digite em que dia estamos (Ex: 01): "))
+mes = str(input("Digite em que mes estamos (Ex: 05): "))
+ano = str(input("Digite em que ano estamos (Ex: 2000): "))
+print(f"A data de hoje é: {dia}/{mes}/{ano}")
